913eb61053ee1893ba4e445c4776981ccb801876
hndrr <53109993+hndrr@users.noreply.github.com>
2020-02-29 21:20:22 +0900

    ReactiveData
    aaa

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,15 +1,29 @@
 <template>
-  <main>{{ vueNumber }}</main>
+  <main>
+    <h2 @click="alerter()">{{ name }}</h2>
+    <h2 @click="alerter2('aaaaaa')">{{ name }}2</h2>
+    <h2 @click="alerter2(vueNumber)">{{ name }}3</h2>
+  </main>
 </template>
 
 <script>
-const number = 2 * 10;
+const number = 2 * 19;
 console.log(number);
 export default {
   data() {
     return {
-      vueNumber: number
+      //ReativeなData
+      vueNumber: number,
+      name: "hndr"
     };
+  },
+  methods: {
+    alerter() {
+      alert(this.vueNumber);
+    },
+    alerter2(value) {
+      alert(value);
+    }
   }
 };
 </script>
